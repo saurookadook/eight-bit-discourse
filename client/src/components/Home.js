29ff328f9228a58fe87a46929b8fb8a9d32e6a5d
saurookadook <maskiella@gmail.com>
2018-06-27 18:48:16 -0400

    Continue initial setup and start building components

---
+++ client/src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NavBar.css'
+
+export const Home = () =>
+    <div className="Home">
+      <h1 className="appName">Welcome to 8-bit Discourse!</h1>
+      <p className="description">A place for open discourse about games.</p>
+    </div>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <NavLink className="NavLink" to="/">Home</NavLink>
+      <NavLink className="NavLink" to="/posts">Latest Posts</NavLink>
+      <NavLink className="NavLink" to="/users/1/posts">Your Posts</NavLink>
+    </div>
+  );
+};
+
+export default NavBar;
