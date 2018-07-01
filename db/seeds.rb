8a1395ce843bf5bb43dff138076df61c9330e351
saurookadook <maskiella@gmail.com>
2018-07-01 15:54:23 -0400

    Add additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,30 @@ post3.save
 
 comment1 = post1.comments.build(user_id: 2, content: "NO WAY IT WAS THE GREATEST GAME EVAR MADE!!!!!!")
 comment1.save
+
+comment2 = post1.comments.build(user_id: 3, content: "Go home; you're drunk.")
+comment2.save
+
+comment3 = post2.comments.build(user_id: 2, content: "what are you smokin, man?")
+comment3.save
+
+comment4 = post2.comments.build(user_id: 3, content: "^ bumped")
+comment4.save
+
+comment5 = post2.comments.build(user_id: 1, content: "SMOKIN MINECRAFT. ALL DAY, EVERY DAY")
+comment5.save
+
+comment6 = post2.comments.build(user_id: 2, content: "....i think you might need help")
+comment6.save
+
+comment7 = post3.comments.build(user_id: 1, content: "NOPE, MINECRAFT IS STILL THE BEST")
+comment7.save
+
+comment8 = post3.comments.build(user_id: 3, content: "Whatever you say, bub....")
+comment8.save
+
+comment9 = post3.comments.build(user_id: 1, content: "YEAH, I DID SAY IT WHICH MEANS IT'S TRUE")
+comment9.save
+
+comment10 = post3.comments.build(user_id: 2, content: "just STOP. TALKING.")
+comment9.save
