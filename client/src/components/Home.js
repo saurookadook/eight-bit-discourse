50a0ab1518d08ecb65817dad58b4c8a68f4bb590
saurookadook <maskiella@gmail.com>
2018-07-01 19:58:00 -0400

    Continue work on displaying image in Home page div

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,8 @@ import '../styles/css/App.css';
 
 export const Home = () =>
     <div className="Home bg-warning pb-3">
+      <img className="homeImage" src="https://images.pexels.com/photos/163036/mario-luigi-yoschi-figures-163036.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
       <h1 className="appName text-left ml-4">8-bit Discourse</h1>
       <p className="description text-left ml-4">A place for open discourse about games.</p>
-      <div className="homeImage"></div>
+
     </div>
