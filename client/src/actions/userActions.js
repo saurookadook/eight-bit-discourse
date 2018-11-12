1e6180149b914b8dfa062383f044cf217ff173d3
saurookadook <maskiella@gmail.com>
2018-11-12 17:26:50 -0500

    Stub out Account page

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -84,19 +84,19 @@ export const authenticate = (credentials) => {
   }
 }
 
-// export const getUser = (credentials) => {
-//   const request = new Request(`${API_URL}/find_user`, {
-//     method: "POST",
-//     headers: new Headers({
-//       "Content-Type": "application/json",
-//       "Authorization": `Bearer ${localStorage.token}`,
-//     }),
-//     body: JSON.stringify({user: credentials})
-//   })
-//   return fetch(request)
-//     .then(response => response.json())
-//     .then(userJson => {return userJson})
-//     .catch(error => {
-//       return error;
-//     });
-// }
+export const getUser = (credentials) => {
+  const request = new Request(`${API_URL}/find_user`, {
+    method: "POST",
+    headers: new Headers({
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${localStorage.token}`,
+    }),
+    body: JSON.stringify({user: credentials})
+  })
+  return fetch(request)
+    .then(response => response.json())
+    .then(userJson => {return userJson})
+    .catch(error => {
+      return error;
+    });
+}
---
+++ client/src/components/Account.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+
+// import { PasswordForgetForm } from './PasswordForget';
+// import PasswordChangeForm from './PasswordChange';
+// import withAuthorization from './withAuthorization';
+
+const AccountPage = ({ authUser }) =>
+  <div>
+    <h1>Account: {authUser.email}</h1>
+    <PasswordForgetForm />
+    <PasswordChangeForm />
+  </div>
+
+const mapStateToProps = (state) => ({
+  authUser: state.sessionState.authUser
+});
+
+const authCondition = (authUser) => !!authUser;
+
+export default compose(
+  withAuthorization(authCondition),
+  connect(mapStateToProps)
+)(AccountPage);
